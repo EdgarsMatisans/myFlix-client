08c3b2070363dacd8dc05082154ec298b0ecb4d6
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2021-12-13 22:07:36 +0100

    bug repairs

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
 import axios from 'axios';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { LoginView } from "../login-view/login-view";
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                {
-                    _id: "619815065345f8c3362abc82",
-                    Title: 'Shrek',
-                    Description: 'A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back.',
-                    ImagePath: "https://www.imdb.com/title/tt0126029/mediaviewer/rm955136512/?ref_=tt_ov_i"
-                },
-                {
-                    _id: "619815a45345f8c3362abc83",
-                    Title: "Shrek 2",
-                    Description: "Shrek and Fiona travel to the Kingdom of Far Far Away, where Fiona's parents are King and Queen, to celebrate their marriage. When they arrive, they find they are not as welcome as they thought they would be.",
-                    ImagePath: "https://www.imdb.com/title/tt0298148/mediaviewer/rm183384576/?ref_=tt_ov_i"
-                },
-                {
-                    _id: "6198176d5345f8c3362abc85",
-                    Title: "Shrek the Third",
-                    Description: "Reluctantly designated as the heir to the land of Far, Far Away, Shrek hatches a plan to install the rebellious Artie as the new king while Princess Fiona tries to fend off a coup d'état by the jilted Prince Charming.",
-                    ImagePath: "https://www.imdb.com/title/tt0413267/mediaviewer/rm4214262784/?ref_=tt_ov_i"
-                }
-
-            ],
+            movies: [],
         }
     }
 
-    componentDidMount() {
-        axios.get('https://mysterious-refuge-92228.herokuapp.com/movies')
+    getMovies(token) {
+        axios.get('https://mysterious-refuge-92228.herokuapp.com/L/movies', {
+            headers: { Authorization: `Bearer ${token}` }
+        })
             .then(response => {
+                // Assign the result to the state
                 this.setState({
                     movies: response.data
                 });
             })
-            .catch(error => {
+            .catch(function (error) {
                 console.log(error);
             });
+    }
+
+    componentDidMount() {
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
+            });
+            this.getMovies(accessToken);
+        }
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -50,21 +50,53 @@ export class MainView extends React.Component {
         });
     }
 
+    onLoggedIn(authData) {
+        console.log(authData);
+        this.setState({
+            user: authData.user.Username
+        });
+
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        this.getMovies(authData.token);
+    }
+
+    onLoggedOut() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        this.setState({
+            user: null
+        });
+    }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, user } = this.state;
 
+        if (!user) return <Row>
+            <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+        </Row>
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
-            <div className="main-view">
-                {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
-                    ))
-                }
-            </div>
+            <Router>
+                <Row className="main-view justify-content-md-center">
+                    <Route exact path="/" render={() => {
+                        return movies.map(m => (
+                            <Col md={3} key={m._id}>
+                                <MovieCard movie={m} />
+                            </Col>
+                        ))
+                    }} />
+                    <Route path="/movies/:movieId" render={({ match }) => {
+                        return <Col md={8}>
+                            <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+                        </Col>
+                    }} />
+
+                </Row>
+            </Router>
         );
     }
 }
