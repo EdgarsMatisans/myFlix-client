d581a66e9fca36a51c4bff61610cde700887d110
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2021-12-25 21:44:01 +0100

    3.6

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,198 +1,6 @@
-// import React from 'react';
-// import axios from 'axios';
-
-// import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-
-// import { MovieCard } from '../movie-card/movie-card';
-// import { MovieView } from '../movie-view/movie-view';
-// import { LoginView } from '../login-view/login-view';
-// import { RegistrationView } from '../registration-view/registration-view';
-// // import { DirectorView } from '../director-view/director-view';
-// // import { GenreView } from '../genre-view/genre-view';
-// // import { Userview } from '../user-view/user-view';
-
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-
-// export class MainView extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             movies: [],
-//         }
-//     }
-
-//     componentDidMount() {
-//         let accessToken = localStorage.getItem('token');
-//         if (accessToken !== null) {
-//             this.setState({
-//                 user: localStorage.getItem('user')
-//             });
-//             this.getMovies(accessToken);
-//         }
-//     }
-
-//     getMovies(token) {
-//         axios.get('https://mysterious-refuge-92228.herokuapp.com/movies', {
-//             headers: { Authorization: `Bearer ${token}` }
-//         })
-//             .then(response => {
-//                 // Assign the result to the state
-//                 this.setState({
-//                     movies: response.data
-//                 });
-//             })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     }
-
-
-
-//     setSelectedMovie(newSelectedMovie) {
-//         this.setState({
-//             selectedMovie: newSelectedMovie
-//         });
-//     }
-
-//     onLoggedIn(authData) {
-//         console.log(authData);
-//         this.setState({
-//             user: authData.user.Username
-//         });
-
-//         localStorage.setItem('token', authData.token);
-//         localStorage.setItem('user', authData.user.Username);
-//         this.getMovies(authData.token);
-//     }
-
-//     onLoggedOut() {
-//         localStorage.removeItem('token');
-//         localStorage.removeItem('user');
-//         this.setState({
-//             user: null
-//         });
-//         window.open("/", "_self");
-//     }
-
-//     render() {
-//         const { movies, user } = this.state;
-
-//         if (!user)
-//             return <Row>
-//                 <Col>
-//                     <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
-//                 </Col>
-//             </Row>
-//         if (movies.length === 0) return <div className="main-view" />;
-
-//         return (
-//             <Router>
-//                 <Row className="main-view justify-content-md-center">
-//                     <Route exact path="/" render={() => {
-//                         if (!user)
-//                             return
-//                         <Col>
-//                             <LoginView onLoggedIn={user =>
-//                                 this.onLoggedIn(user)} />
-//                         </Col>
-//                         // Before the movies have been loaded
-//                         if (movies.length === 0) return <div className="main-view"></div>
-//                         return movies.map(m => (
-//                             <Col md={3} key={m._id}>
-//                                 <MovieCard movie={m}
-//                                 />
-//                             </Col>
-//                         ))
-//                     }}
-//                     />
-
-//                     <Route path="/register" render={() => {
-//                         if (user)
-//                             return <Redirect to="/" />
-//                         return <Col>
-//                             <RegistrationView />
-//                         </Col>
-//                     }}
-//                     />
-
-//                     <Route path="/movies/:movieId" render={({ match, history }) => {
-//                         if (!user)
-//                             return
-//                         <Col>
-//                             <LoginView onLoggedIn={user =>
-//                                 this.onLoggedIn(user)}
-//                             />
-//                         </Col>
-//                         if (movies.length === 0) return <div className="main-view" />;
-//                         return <Col md={8}>
-//                             <MovieView movie={movies.find(m => m._id === match.params.movieId)}
-//                                 onBackClick={() => history.goBack()}
-//                             />
-//                         </Col>
-//                     }}
-//                     />
-
-//                     <Route
-//                         path="/directors/:Name"
-//                         render={({ match, history }) => {
-//                             if (!user)
-//                                 return (
-//                                     <LoginView onLoggedIn={(user) =>
-//                                         this.onLoggedIn(user)} />
-//                                 );
-//                             if (movies.length === 0) return <div className="main-view" />;
-//                             return (
-//                                 <Col md={8}>
-//                                     <DirectorView
-//                                         Director={
-//                                             movies.find(
-//                                                 (m) => m.Director.Name === match.params.Name).Director
-//                                         }
-//                                         movies={movies}
-//                                         onBackClick={() => history.goBack()}
-//                                     />
-//                                 </Col>
-//                             );
-//                         }}
-//                     />
-
-//                     <Route
-//                         path="/genres/:Name"
-//                         render={({ match, history }) => {
-//                             if (!user) return (
-//                                 <LoginView onLoggedIn={(user) =>
-//                                     this.onLoggedIn(user)}
-//                                 />
-//                             );
-//                             if (movies.length === 0) return <div className="main-view" />;
-//                             return (
-//                                 <Col md={8}>
-//                                     <GenreView
-//                                         movies={movies}
-//                                         Genre={
-//                                             movies.find((m) => m.Genre.Name === match.params.Name).Genre
-//                                         }
-//                                         onBackClick={() => history.goBack()}
-//                                     />
-//                                 </Col>
-//                             );
-//                         }}
-//                     />
-
-
-//                 </Row>
-//             </Router>
-//         );
-//     }
-// }
-
-// export default MainView;
-
-
-
 import React from "react";
 import axios from "axios";
+import { connect } from 'react-redux';
 
 import {
     BrowserRouter as Router,
@@ -200,6 +8,8 @@ import {
     Redirect,
     Routes,
 } from "react-router-dom";
+import { setMovies } from '../../actions/actions';
+// import MoviesList from '../movies-list/movies-list';
 
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
@@ -217,7 +27,7 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [],
+            user: null
         };
     }
 
@@ -236,21 +46,13 @@ export class MainView extends React.Component {
             .get("https://mysterious-refuge-92228.herokuapp.com/movies", {
                 headers: { Authorization: `Bearer ${token}` },
             })
-            .then((response) => {
+            .then(response => {
                 // Assign the result to the state
-                this.setState({
-                    movies: response.data,
-                });
+                this.props.setMovies(response.data);
             })
             .catch(function (error) {
                 console.log(error);
             });
-    }
-
-    setSelectedMovie(newSelectedMovie) {
-        this.setState({
-            selectedMovie: newSelectedMovie,
-        });
     }
 
     onLoggedIn(authData) {
@@ -273,45 +75,39 @@ export class MainView extends React.Component {
         window.open("/", "_self");
     }
 
-    render() {
-        const { movies, user } = this.state;
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie,
+        });
+    }
 
-        // if (!user)
-        //   return (
-        //     <Row>
-        //       <Col>
-        //         <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
-        //       </Col>
-        //     </Row>
-        //   );
-        // if (movies.length === 0) return <div className="main-view" />;
+    render() {
+        let { movies } = this.props;
+        let { user } = this.state;
 
         return (
             <Router>
                 <Row className="main-view justify-content-md-center">
+                    {/* route for link on main-view to movie-card, otherwise the login-view. */}
                     <Route
                         exact
                         path="/"
                         render={() => {
-                            if (!user) {
+                            if (!user)
                                 return (
                                     <Col>
-                                        <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                                        <LoginView
+                                            onLoggedIn={(user) => this.onLoggedIn(user)}
+                                        />
                                     </Col>
                                 );
-                            }
-
-                            // Before the movies have been loaded
-                            if (movies.length === 0) return <div className="main-view"></div>;
-                            return movies.map((m) => (
-                                <Col md={3} key={m._id}>
-                                    <MovieCard movie={m} />
-                                </Col>
-                            ));
+                            if (movies.length === 0) return <div className="main-view" />;
+                            return <MovieList movies={movies} />;
                         }}
                     />
-
+                    {/* route for link on main-view to register-view */}
                     <Route
+                        exact
                         path="/register"
                         render={() => {
                             if (user) return <Redirect to="/" />;
@@ -402,4 +198,9 @@ export class MainView extends React.Component {
     }
 }
 
-export default MainView;
+let mapStateToProps = state => {
+    return { movies: state.movies }
+}
+
+export default connect(mapStateToProps,
+    { setMovies })(MainView);
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -7,6 +7,7 @@ import Route from "react-router-dom"
 import Router from "react-router-dom"
 import BrowserRouter from "react-router-dom";
 import Container from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 // SCSS Import
 // import "./registration-view.scss"
 
@@ -15,6 +16,12 @@ export function RegistrationView(props) {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const [birthday, setBirthday] = useState('');
+
+    const [usernameError, setUsernameError] = useState({});
+    const [passwordError, setPasswordError] = useState({});
+    const [emailError, setEmailError] = useState({});
+    const [birthdateError, setBirthdateError] = useState({});
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -34,87 +41,149 @@ export function RegistrationView(props) {
             });
     };
 
+    const formValidation = () => {
+        let usernameError = {};
+        let passwordError = {};
+        let emailError = {};
+        let birthdateError = {};
+        let isValid = true;
+
+        if (username === '') {
+            usernameError.usernameEmpty = "Please enter your username.";
+            isValid = false;
+        }
+
+        if (username.trim().length < 5) {
+            usernameError.usernameShort = "Username needs to be at least 5 characters long.";
+            isValid = false;
+        }
+
+        if (password.trim().length < 5) {
+            passwordError.passwordShort = "Password needs to be at least 5 characters long.";
+            isValid = false;
+        }
+
+        if (!(email && email.includes(".") && email.includes("@"))) {
+            emailError.emailNotEmail = "Please enter correct email address.";
+            isValid = false;
+        }
+
+        setUsernameError(usernameError);
+        setPasswordError(passwordError);
+        setEmailError(emailError);
+        setBirthdateError(birthdateError);
+        return isValid;
+    };
+
     return (
-        <Container>
-            <Form>
+        <Form className="register-view justify-content-md-center">
+            <Container>
+                <Form>
 
-                <Form.Group
-                    controlId="formBasicUsername">
-                    <Form.Label>
-                        Username:
-                    </Form.Label>
-                    <Form.Control
-                        className="username"
-                        value={username}
-                        type="text"
-                        placeholder="Create Username"
-                        onChange={e =>
-                            setUsername(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group
+                        controlId="formBasicUsername">
+                        <Form.Label>
+                            Username:
+                        </Form.Label>
+                        <Form.Control
+                            className="username"
+                            value={username}
+                            type="text"
+                            placeholder="Create Username"
+                            onChange={e =>
+                                setUsername(e.target.value)}>
+                        </Form.Control>
+                        {Object.keys(usernameError)
+                            .map((key) => {
+                                return (
+                                    <div key={key}>
+                                        {usernameError[key]}
+                                    </div>
+                                );
+                            })}
+                    </Form.Group>
 
-                <Form.Group
-                    controlId="formBasicPassword">
-                    <Form.Label>
-                        Password:
-                    </Form.Label>
-                    <Form.Control
-                        className="password"
-                        value={password}
-                        type="text"
-                        placeholder="Create Password"
-                        onChange={e =>
-                            setPassword(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group
+                        controlId="formBasicPassword">
+                        <Form.Label>
+                            Password:
+                        </Form.Label>
+                        <Form.Control
+                            className="password"
+                            value={password}
+                            type="text"
+                            placeholder="Create Password"
+                            onChange={e =>
+                                setPassword(e.target.value)}>
+                        </Form.Control>
+                        {Object.keys(passwordError)
+                            .map((key) => {
+                                return (
+                                    <div key={key}>
+                                        {passwordError[key]}
+                                    </div>
+                                );
+                            })}
+                    </Form.Group>
 
-                <Form.Group
-                    controlId="formBasicBirthday">
-                    <Form.Label>
-                        Birthday:
-                    </Form.Label>
-                    <Form.Control
-                        className="birthday"
-                        value={birthday}
-                        type="date"
-                        placeholder="Enter Birthday"
-                        onChange={e =>
-                            setBirthday(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group
+                        controlId="formBasicBirthday">
+                        <Form.Label>
+                            Birthday:
+                        </Form.Label>
+                        <Form.Control
+                            className="birthday"
+                            value={birthday}
+                            type="date"
+                            placeholder="Enter Birthday"
+                            onChange={e =>
+                                setBirthday(e.target.value)}>
+                        </Form.Control>
+                        {Object.keys(birthdateError)
+                            .map((key) => {
+                                return (
+                                    <div key={key}>
+                                        {birthdateError[key]}
+                                    </div>
+                                );
+                            })}
+                    </Form.Group>
 
-                <Form.Group
-                    controlId="formBasicEmail">
-                    <Form.Label>
-                        Email:
-                    </Form.Label>
-                    <Form.Control
-                        className="email"
-                        value={email}
-                        type="email"
-                        placeholder="Enter Email"
-                        onChange={e =>
-                            setEmail(e.target.value)}>
-                    </Form.Control>
-                    <Form.Text
-                        className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                    <Form.Group
+                        controlId="formBasicEmail">
+                        <Form.Label>
+                            Email:
+                        </Form.Label>
+                        <Form.Control
+                            className="email"
+                            value={email}
+                            type="email"
+                            placeholder="Enter Email"
+                            onChange={e =>
+                                setEmail(e.target.value)}>
+                        </Form.Control>
+                        <Form.Text
+                            className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                        {Object.keys(emailError)
+                            .map((key) => {
+                                return (
+                                    <div key={key}>
+                                        {emailError[key]}
+                                    </div>
+                                );
+                            })}
+                    </Form.Group>
 
-                <Form.Group
-                    controlId="formBasicCheckbox">
-                    <Form.Check
-                        type="checkbox"
-                        label="Check me out" />
-                </Form.Group>
-
-                <Button
-                    variant="primary"
-                    type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </Container>
+                    <Button
+                        variant="primary"
+                        type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        </Form>
     );
 }
+
